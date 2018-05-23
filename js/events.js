86c0877045a3427375662365aaf7e99052305f78
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-23 09:14:53 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -21,6 +21,8 @@ function frameIt() {
 
 function pressIt() {
   $('form').on('keydown', function(key) {
-    if (key.which)
+    if (key.which == 71){
+      alert('You have pressed the G key')
+    }
   })
 }
